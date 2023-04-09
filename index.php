29f300b6f33d17e96c3fdcface8fcf1bd198b744
Chris Rickard <103567821+rickardc@users.noreply.github.com>
2023-04-09 15:27:37 +1000

    update menu

--- index.php
+++ index.php
@@ -4,31 +4,17 @@
 
     <head>
         <meta charset="utf-8" />
-        <meta name="description" content="Information about TCP & IP" />
-        <meta name="keywords" content="TCP, IP, Transmission Control Protocol, quiz" />
+        <meta name="description" content="Photo Album" />
+        <meta name="keywords" content="photos, images, photo album" />
         <meta name="author" content="Chris Rickard" />
-        <title>Information about TCP &amp; IP - Admin</title>
         <link href="styles/style.css" rel="stylesheet">
-        <title>Photo Album</title>
+        <title>Photo Album Index</title>
 
     </head>
 
     <body>
-
-        <h3>Photo Uploader</h3>
-        <a href="photouploader.php">upload.php</a>
-
-        <br>
-        <br>
-
-        <h3>Photo Album</h3>
-        <a href="photoalbum.php">photoalbum.php</a>
-
-        <br>
-        <br>
-        
-        <h3>Photo Lookup</h3>
-        <a href="photolookup.php">photolookup.php</a>
-
+        <?php
+            require('menu.php');
+            ?>
 
         </body>
